34bf98ebf7867cc023a5d8140f7a981095b1d5ce
Ayushi <ayushi@gmail.com>
2025-08-11 12:04:18 -0400

    testing commit

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ model = joblib.load("lr_model.joblib")
 #Displays a large title on the web app
 st.title("Fake News Detector")
 
+
+
 #Adds some instructional text 
 st.write("Enter a News Article below to check whether it is Fake or Real. ")
 
